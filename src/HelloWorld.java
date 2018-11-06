dd82a7c0a76af9bf613b0a26dda1946a398b7bcc
Taehoon Kee <taehoon.kee@gmail.com>
2018-11-06 10:50:59 +0900

    added real java "Hello World" project...

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tkee
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
